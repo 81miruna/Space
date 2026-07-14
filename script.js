1312de0d0cd74d48f4c35a079a2143ddfe83fc0b
81miruna <mirunateodora0204@gmail.com>
2026-07-14 17:22:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
    ========================================================= */
 
 document.addEventListener('DOMContentLoaded', () => {
+   <script data-goatcounter="https://miruna.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
 
   /* ============ 1. STARFIELD BACKGROUND ============ */
   (function starfield(){
